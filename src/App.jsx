a67a476441c3b7aabf11836b6fc02d1ac9077cca
Lucas Rocha <lucasbezerrar@gmail.com>
2024-05-11 18:31:07 -0300

    Add estilo de lista dos professores

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { HomePage } from './pages/HomePage/HomePage';
+import { ListPage } from './pages/ListPage/ListPage';
 import './App.css';
 
 export const App = () => {
   return (
     <>
-      <HomePage />
+      <ListPage />
     </>
   );
 };
